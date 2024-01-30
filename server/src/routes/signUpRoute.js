c1e7ef46b6721c4f6100f19089ef7808f7d1d2e8
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-01-30 05:31:42 -0600

    added some code to salt and hash passwords

--- server/src/routes/signUpRoute.js
+++ server/src/routes/signUpRoute.js
@@ -1,5 +1,5 @@
 import { getDbConnection } from "../db";
-
+import bcrypt from "bcrypt"
 
 // describe the route to our server using properties (for now)
 export const signUpRoute ={
@@ -14,14 +14,23 @@ export const signUpRoute ={
         const {email, password} = req.body
         if(!email || !password) return res.sendStatus(500);
 
+
+        // define salt rounds (bcrypt algo that scales depending on system)
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        
         // Debug log 
+        // get visual on our password before and after.
+        console.log(`Password:${password}\nSalt: ${salt}\nHash:${passwordHash}`);
         console.log(`Email: ${email}, password: ${password}`)
 
         const db = getDbConnection('ecommerce')
         // fyi: collections are just like folders in a nosql database or tables in sql
         const result = await db.collection("users").insertOne({
             email,
-            password
+            passwordHash
         })
         if(!result) return res.sendStatus(500);
 
