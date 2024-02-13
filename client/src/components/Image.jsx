97bb30aeebc380ac9202dcc5e12206731843be83
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-02-12 19:03:59 -0600

    added some assets as well as an Image component

---
+++ client/src/components/Image.jsx
@@ -0,0 +1,20 @@
+import Placeholder from "../assets/industrialninja.gif";
+
+export const Image = ({src=Placeholder, title, alt, style})  => {
+    
+   
+    return (
+        <div
+        style={{
+            display: "flex",
+            placeItems: "center",
+            border: "1px solid lightgray",
+            minHeight:"200px",
+            backgroundColor: "white"
+        }}
+        >
+            {/* accept from props and import styles with spread */}
+            <img  src={src} title={title} alt={alt} style={{width:"100%", ...style}} />
+    </div>
+    )
+}
