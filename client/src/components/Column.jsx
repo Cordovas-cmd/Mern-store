63a1042867f52e4a43588028f8a6372a297f6005
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-02-12 18:35:44 -0600

    removed some initial styling and added some custom styling as well as column component

---
+++ client/src/components/Column.jsx
@@ -0,0 +1,18 @@
+// takes two props
+export const Column = ({children, style}) => {
+return (
+    <div
+    // apply regular react styling
+        style={{
+            display: flex,
+            flexDirection: "column",
+            gap: "8px",
+            justifyContent:"flex-start",
+            // spread any extra styling
+            ...style,
+        }}
+        >
+            {children}
+    </div>
+)
+}
