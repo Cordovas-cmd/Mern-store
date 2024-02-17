82dfef5952fde8ae4f3a99ca1298821e33c948f6
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-02-17 17:18:05 -0600

    added modal component

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,46 @@
+import {Column} from "./Column"
+import {Row} from "./Row"
+
+export const Modal = ({ children, setVisibility}) => {
+    return (
+        <div>
+        // overlay displayed behind the modal
+        <div 
+        style={{
+            backgroundColor: "rgba(0,0,0,0.8)",
+            width: "100vw",
+            height: "100vh",
+            zIndex: "0",
+            top:  "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            position: "absolute",
+
+        }}
+        // define wether we see it or not, if we click bg it will turn off
+        onClick={() => setVisibility(false)}
+        >
+        </div>
+        <Column 
+        style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            minWidth: "60vw",
+            maxHeight: "90vh",
+            transform: "(-50%, -50%)",
+            backgroundColor: "black",
+            border: "2px solid white",
+            borderRadius: "8px",
+            padding: "16px",
+
+        }}
+        >
+            <Row style={{justifyContent: "flex-end"}}>
+        <button onClick={() => setVisibility(false)}>✖</button>
+            </Row>
+            {children}
+        </Column>
+        </div>
+    )
+}
