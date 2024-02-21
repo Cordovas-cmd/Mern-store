76f4e7c211dc63a067eaca617d608667875b94c0
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-02-21 14:33:36 -0600

    added the option to zoom in on an image when clicked

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,20 +1,30 @@
 // import Placeholder from "../assets/industrialninja.gif";
-
+import {useState} from "react";
+import { ImageZoom } from "./ImageZoom"
 export const Image = ({src=Placeholder, title, alt, style})  => {
     
+    // zoom version starts off as not visible
+    const [isVisible, setVisibility] = useState(false);
    
     return (
+        <>
         <div
         style={{
             display: "flex",
             placeItems: "center",
             border: "1px solid lightgray",
             minHeight:"200px",
-            backgroundColor: "white"
+            backgroundColor: "white",
+            cursor: "pointer"
         }}
+        // when image is clicked set modal visibility to true
+        onClick={() => setVisibility(true)}
         >
             {/* accept from props and import styles with spread */}
             <img  src={src} title={title} alt={alt} style={{width:"100%", ...style}} />
     </div>
+    {/* hit ctrl space to see props */}
+    <ImageZoom isVisible={isVisible} setVisibility={setVisibility} src={src} alt={alt} title={title} />
+            </>
     )
 }
---
+++ client/src/components/ImageZoom.jsx
@@ -0,0 +1,22 @@
+import { Modal } from "./Modal"
+
+
+export const ImageZoom = ({ src, title, alt, isVisible, setVisibility}) => {
+    return (
+        // react fragments = <> </>
+        <>
+       { isVisible &&( <Modal setVisibility={setVisibility}>
+            <div
+            style={{backgroundColor: "white"}}>
+                <img src={src} title={title}alt={alt}
+                style={{
+                    height:"80vh",
+                    maxWidth:"90vw",
+                    backgroundColor: "white",
+                }} />
+
+            </div>
+        </Modal>)}
+                </>
+    )
+}
