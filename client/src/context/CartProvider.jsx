1039bbe54258036be956241e0d0c059ca2f7be13
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-03-19 18:11:03 -0500

    added the local storage functionality to allow the cart items to persist, took a while debugging.. ended up being a cache issue

--- client/src/context/CartProvider.jsx
+++ client/src/context/CartProvider.jsx
@@ -1,17 +1,29 @@
 // provide cart info for all other components
-import { useState } from "react";
+// use effect is a hook that helps us tie changes to a certain state variable
+import { useState, useEffect } from "react";
 import { createContext } from "react";
 
 // context that needs to be managed.
 export const CartContext = createContext();
 
+
 export const CartProvider = ({children}) => {
-    const  [cartItems, setCartItems] = useState([])
+    
+const [cartItems, setCartItems] = useState(() => 
+JSON.parse(localStorage.getItem("cartItems") || [])
+);
+
+// use Effect will take two arguements, the first is a function, the second is a dependancy array. 
+// whenever cart items change please execute this function.
+useEffect(() => localStorage.setItem("cartItems", JSON.stringify(cartItems)), 
+[cartItems]
+);
+
 
     return (
         // 
         <CartContext.Provider value={[cartItems, setCartItems]}>
             {children}
         </CartContext.Provider>
-    )
-}
+    );
+};
