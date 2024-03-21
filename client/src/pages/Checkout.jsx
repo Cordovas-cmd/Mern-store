f71156baba25075008f0842ff5399771485c150a
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-03-21 17:28:47 -0500

    added a confirm modal as well as a confirm (page) whenever I am done checking out, with the option to continue shopping

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,5 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import { Column } from "../components/Column"
+import { useState } from "react"
+import { CartItemsList } from "../components/CartItemsList";
+import { Row } from "../components/Row"
+import { ConfirmModal } from "../components/ConfirmModal";
+// want to view a summary of my order, a confirmation button as well as my information. 
+
 export const CheckOut = () => {
+    // check if order has been confirmed
+    const [isConfirmed, setIsConfirmed] = useState(false);
+
+    const navigate= useNavigate();
     return (
-        <p>Checkout Page</p>
+        <Column>
+        {isConfirmed ? (
+            <p>Thank you, your order has been confirmed</p>
+        ) : (
+            <Column>
+            <h2>User Info</h2>
+            <CartItemsList/>
+            <Row style={{justifyContent:"center"}}>
+                <ConfirmModal 
+                label="Confirm Order"
+                setIsConfirmed={setIsConfirmed}/>
+                <button onClick={()=> navigate("/")}>Continue Shopping</button>
+            </Row>
+            </Column>
+        )}
+        </Column>
     )
-} 
+};
