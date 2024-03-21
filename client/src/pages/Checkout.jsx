52e01d4fe1a82f790e44dcf6238024a5ac4838c1
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-03-21 17:46:24 -0500

    added the confirm page after modal button is clicked, redirects user to homepage and clears the cart from local storage

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,20 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import { Column } from "../components/Column"
-import { useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import { CartItemsList } from "../components/CartItemsList";
 import { Row } from "../components/Row"
 import { ConfirmModal } from "../components/ConfirmModal";
+import {CartContext} from "../context/CartProvider"
 // want to view a summary of my order, a confirmation button as well as my information. 
 
 export const CheckOut = () => {
+    const waitTime = 5 * 1000;
+    const navigate= useNavigate();
+    const [, setCartItems]  = useContext(CartContext)
+    const [timeRemaining, setTimeRemaining] = useState(waitTime / 1000)
     // check if order has been confirmed
     const [isConfirmed, setIsConfirmed] = useState(false);
 
-    const navigate= useNavigate();
+    useEffect(()=> {
+        if(isConfirmed){
+            setCartItems([]);
+                setTimeout(()=> navigate("/"), waitTime)
+                // subtract 1 from time remaining per second
+                setInterval(()=> setTimeRemaining(pre => pre - 1), 1000)
+        }
+    }, [isConfirmed])
     return (
         <Column>
         {isConfirmed ? (
-            <p>Thank you, your order has been confirmed</p>
+            <p>Thank you, your order has been confirmed. You will be directed to the homepage in {timeRemaining} seconds...</p>
         ) : (
             <Column>
             <h2>User Info</h2>
