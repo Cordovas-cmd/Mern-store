d92b7353c3f02a0e1328606750e9248185bc33e7
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-03-26 16:29:27 -0500

    added the login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import {Modal} from "./Modal"
 import { InputField } from "./InputField";
-
+import axios from "axios";
+import { CurrentUserContext } from "../context/CurrentUserProvider";
 export const LogIn= ()=> {
+    const [  , , setToken] = useContext(CurrentUserContext);
     // set state so the browser will re render the UI when variable changes. (set to false so it's not visible)
     const [isVisible, setVisibility] = useState(false);
     // set email to empty string at beginning
@@ -14,7 +16,19 @@ export const LogIn= ()=> {
         <Modal setVisibility={setVisibility}>
             <InputField label="Email:" setValue={setEmail} value={email}/>
             <InputField label="Password:" setValue={setPassword} value={password} type="password"/>
-            <button>Log In</button>
+            <button onClick={ async ()=>{
+              try {
+                // when using axios the specify the method and the url/ object/data we want to send
+                const response = await axios.post("http://localhost:3001/api/sign-in", { email, password } )
+
+                const {data:{token}} = response;
+                // console.log(token);               
+                setToken(token);
+                setVisibility(false);
+            } catch(e) {
+                    console.log(e);
+            }   
+            }}>Log In</button>
         </Modal>
     )
 }
